f9a6cc82cef47183ee086309e142449a998df704
Lulu300 <ludwigsimon77@gmail.com>
2017-06-09 23:42:51 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+def install():
+    desc = 'A Python service to sync what you watch!'
+    setup(
+        name='shows_sync',
+        version='0.1',
+        description=desc,
+        long_description=desc,
+        author='PySyncer',
+        author_email='',
+        url='https://github.com/PySyncer/shows_sync',
+        classifiers=['Operating System :: POSIX',
+                     'Operating System :: Microsoft :: Windows',
+                     'Operating System :: MacOS :: MacOS X',
+                     'Programming Language :: Python',
+                     'Programming Language :: Python :: 3.2',
+                     'Programming Language :: Python :: 3.3',
+                     'Programming Language :: Python :: 3.4',
+                     'Programming Language :: Python :: 3.5',
+                     'Programming Language :: Python :: 3.6',
+                     'Programming Language :: Python :: 3 :: Only'
+                     ],
+        packages=find_packages(),
+        install_requires=[
+            'requests',
+            'plexapi',
+            'bs4tmdbsimple'
+        ],
+    )
+
+
+if __name__ == "__main__":
+    install()
