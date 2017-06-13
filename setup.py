4250ee7ce9c92fc3a65fdff5f9267d7586a3f329
Ludwig <ludwigsimon77@gmail.com>
2017-06-14 00:09:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
+import os
 from setuptools import setup, find_packages
 
 
 def install():
     desc = 'A Python service to sync what you watch!'
+    ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__)))
+
+    with open(os.path.join(ROOT, 'readme.md'), 'r') as r:
+    long_description = r.read()
+
     setup(
         name='shows_sync',
         version='0.1',
         description=desc,
-        long_description=desc,
+        long_description=long_description,
         author='PySyncer',
         author_email='',
         url='https://github.com/PySyncer/shows_sync',
