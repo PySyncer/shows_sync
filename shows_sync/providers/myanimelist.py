438b7a230583499318ee3a72247cb8dae7a71c4e
Ludwig <ludwigsimon77@gmail.com>
2017-06-25 19:11:47 +0200

    Support mal, need refactor

--- shows_sync/providers/myanimelist.py
+++ shows_sync/providers/myanimelist.py
@@ -4,60 +4,56 @@ import requests
 import constants as CONSTANTS
 import urllib
 import logging
-
+from pymal import Mal
+import sys
 
 class MyAnimeList(object):
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
-
-    def generate_watch_xml(self, episode_number):
-        xml = etree.Element("entry")
-        etree.SubElement(xml, "episode").text = episode_number
-        etree.SubElement(xml, "status").text = '1'
-        return xml
-
-    def mark_as_watched(self, xml, show_id):
-        r = self.request(
-                method='POST',
-                url=CONSTANTS.MAL_ADD_URL.format(show_id),
-                data=[('data', etree.tostring(xml))])
-        if 'is already in the list' in r.text:
-            r = self.request(
-                method='POST',
-                url=CONSTANTS.MAL_UPDATE_URL.format(show_id),
-                data=[('data', etree.tostring(xml))])
-        return True
+        self.mal = Mal.Mal(username, password)
 
     def update(self, episodes):
-        print(episodes)
-        logging.warn('MAL Provider not available, rework in progress')
-        pass
-        # for episode in self.reduce(episodes):
-        #     xml = self.generate_watch_xml(episode['episode'])
-        #     show_id = self.getId(episode['alias'])
-        #     self.mark_as_watched(xml, show_id)
-        #     logging.info('[MAL] Updated {0}, episode {1}.'.format(episode['alias'],
-        #                                                    episode['episode']))
+        for show_key, show in episodes.items():
+            for season_key, season in show['tvdb']['seasons'].items():
+                try:
+                    # Try to get ID of show on MAL
+                    anime = self.mal.anime.search(show['tvdb']['original_title'])[0]
+                    id = anime['id']
+                except:
+                    logging.warning('Cannot find show {0}.'.format(show['tvdb']['original_title']))
+                    break
 
-    def getId(self, title):
-        title = urllib.parse.quote_plus(title)
-        r = self.request(url=CONSTANTS.MAL_SEARCH_URL.format(title)).json()
-        show_id = r['categories'][0]['items'][0]['id']
-        return show_id
+                # IF the total number of epiosde is smaller than the absolute number of the episode,
+                # MAL probably add another show coresponding to the season of the episode, so we will search this show
+                if int(anime['episodes']) < season['episodes'][-1]['absoluteNumber']:
+                    try:
+                        # Search show coresponding to the season
+                        anime = self.mal.anime.search(show['tvdb']['original_title'] + ' ' + season_key)[0]
+                        id = anime['id']
+                    except:
+                        logging.warning('Cannot find show {0}.'.format(show['tvdb']['original_title'] + ' ' + season_key))
+                        break
+                    # Mark as watched with not absolute number
+                    r = self.mark_as_watched(id, season['episodes'][-1]['episode'])
+                    self.watch_log(r, anime['title'], season['episodes'][-1]['episode'])
+                else:
+                    # Mark as watched with absolute number
+                    r = self.mark_as_watched(id, season['episodes'][-1]['absoluteNumber'])
+                    self.watch_log(r, anime['title'], season['episodes'][-1]['absoluteNumber'])
 
-    def request(self, url, data='', method='GET'):
-        r = requests.request(
-            method=method,
-            url=url,
-            data=data,
-            auth=(self.username, self.password)
-            )
+    def watch_log(self, r, title, episode):
+        if r == 'Updated' or r == 'Created':
+            logging.info('{0} {1}, episode {2}.'.format(r, title, episode))
+        else:
+            logging.warning('Cannot update {0}, episode {1} with message : {2}.'.format(title, episode, r))
 
-        if 'Too Many Requests' in r.text:
-            time.sleep(2)
-            self.request(url, data=data)
+    def mark_as_watched(self, id, episode):
+        r = self.mal.anime.add(id, str(episode))
+        if 'is already in the list' in r:
+            logging.debug(r)
+            r = self.mal.anime.update(id, str(episode))
         return r
 
     def reduce(self, history):
