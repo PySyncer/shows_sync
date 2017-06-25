348c362699b5e80c8a23eb2a23d76c2f5efe40a2
Ludwig <ludwigsimon77@gmail.com>
2017-06-25 13:44:46 +0200

    Update TVTime with new dict

--- shows_sync/providers/tvtime.py
+++ shows_sync/providers/tvtime.py
@@ -41,34 +41,34 @@ class TVTIME:
         print('TVTIME] Your account has been linked.')
         return r['access_token']
 
-    def follow(self, show):
+    def follow(self, show, show_key):
         r = self.request(
             method='POST',
             url=CONSTANTS.TVTIME_FOLLOW,
             data={'access_token': self.token,
                   'show_id': show['tvdb']['tvdb_id']})
         if r['result'] == 'OK':
-            logging.info('Follow {0}.'.format(show['tvdb']['show_title']))
+            logging.info('Follow {0}.'.format(show_key))
         else:
             logging.warning('Cannot Follow {0} with message : {1}.'.format(
                             show['tvdb']['show_title'], r['message']))
 
     def update(self, episodes):
         for show_key, show in episodes.items():
-            self.follow(show)
-            for season in show['tvdb']['Seasons']:
+            self.follow(show, show_key)
+            for season_key, season in show['tvdb']['seasons'].items():
                 for episode in season['episodes']:
                     r = self.request(
                             method='POST',
                             url=CONSTANTS.TVTIME_CHECKIN,
                             data={'access_token': self.token,
                                   'show_id': show['tvdb']['tvdb_id'],
-                                  'season_number': season['season'],
+                                  'season_number': season_key,
                                   'number': episode['episode']})
                     if r['result'] == 'OK':
-                        logging.info('Mark as watched {0} season {1} episode {2}'.format(show['tvdb']['show_title'], season['season'], episode['episode']))
+                        logging.info('Mark as watched {0} season {1} episode {2}'.format(show['tvdb']['original_title'], season_key, episode['episode']))
                     else:
-                        logging.warning('Cannot mark as watched {0} season {1} episode {2} with message : {3}.'.format(show['show_title'], season['season'], episode['episode'], r['message']))
+                        logging.warning('Cannot mark as watched {0} season {1} episode {2} with message : {3}.'.format(show_key, season_key, episode['episode'], r['message']))
 
     def request(self, url, method='GET', data={}):
         r = requests.request(
